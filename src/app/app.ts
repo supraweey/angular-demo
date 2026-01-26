7c4bf16f977f0015cc66095f732feecc4588a94c
suprawee.y <suprawee.y@arise.tech>
2026-01-26 11:07:47 +0700

    Add random user when clicking

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+    // this.selectedUser = DUMMY_USERS[randomIndex]
+  }
 }
